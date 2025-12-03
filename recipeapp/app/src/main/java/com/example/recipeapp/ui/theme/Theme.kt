ecd25b1eee0817a9f5e225a28995e3e822260beb
kauas3228 <kauas3228@gmail.com>
2025-12-02 23:38:04 -0300

    style: arrumando cor de fundo tema claro

--- recipeapp/app/src/main/java/com/example/recipeapp/ui/theme/Theme.kt
+++ recipeapp/app/src/main/java/com/example/recipeapp/ui/theme/Theme.kt
@@ -24,7 +24,7 @@ private val LightColorScheme = lightColorScheme(
     secondary = SecondaryColor,
     tertiary = White,
     onPrimary = LightFont,
-    background = LightFont
+    background = LightBg
 )
 
 @Composable
