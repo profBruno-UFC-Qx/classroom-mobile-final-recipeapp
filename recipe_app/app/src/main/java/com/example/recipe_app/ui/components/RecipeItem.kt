76715b7049050ead90733d830c5cba22a03b2e81
kauas3228 <kauas3228@gmail.com>
2025-12-02 21:08:13 -0300

    style: adicionando cores

---
+++ recipe_app/app/src/main/java/com/example/recipe_app/ui/components/RecipeItem.kt
@@ -0,0 +1,2 @@
+package com.example.recipe_app.ui.components
+
--- recipe_app/app/src/main/java/com/example/recipe_app/ui/theme/Theme.kt
+++ recipe_app/app/src/main/java/com/example/recipe_app/ui/theme/Theme.kt
@@ -3,6 +3,7 @@ package com.example.recipe_app.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -12,25 +13,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = PrimaryColor,
+    secondary = SecondaryColor,
+    tertiary = White,
+    onPrimary = DarkFont,
+    background = DarkBg
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = PrimaryColor,
+    secondary = SecondaryColor,
+    tertiary = White,
+    onPrimary = LightFont,
+    background = LightBg
 )
 
 @Composable
