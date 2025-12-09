12a22b62035b6e921e8c04791e7c417495e50f7b
kauas3228 <kauas3228@gmail.com>
2025-12-09 17:14:49 -0300

    fix: ajustando id para ser uma string e receber corretamente os valores das receitas criada

--- recipeapp/app/src/main/java/com/example/recipeapp/data/model/Recipe.kt
+++ recipeapp/app/src/main/java/com/example/recipeapp/data/model/Recipe.kt
@@ -3,7 +3,7 @@ package com.example.recipeapp.data.model
 import com.google.gson.annotations.SerializedName
 
 data class Recipe (
-    val id: Int = 0,
+    val id: String = "",
     val receita: String = "",
     val ingredientes: String = "",
     val modo_preparo: String = "",
---
+++ recipeapp/app/src/main/java/com/example/recipeapp/ui/screens/MyRecipesScreen/MyRecipeViewModel.kt
@@ -0,0 +1,2 @@
+package com.example.recipeapp.ui.screens.MyRecipesScreen
+
