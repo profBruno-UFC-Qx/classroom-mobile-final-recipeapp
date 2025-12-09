8870a18e5865c0c4c9b9b2a406816a3de76ca14b
kauas3228 <kauas3228@gmail.com>
2025-12-08 23:11:10 -0300

    fix: retirando Serialize para não dar conflito com dados do firebase

--- recipeapp/app/src/main/java/com/example/recipeapp/data/model/Recipe.kt
+++ recipeapp/app/src/main/java/com/example/recipeapp/data/model/Recipe.kt
@@ -3,10 +3,10 @@ package com.example.recipeapp.data.model
 import com.google.gson.annotations.SerializedName
 
 data class Recipe (
-    val id: Int,
-    val receita: String,
-    val ingredientes: String,
-    @SerializedName("modo_preparo") val modoPreparo: String,
-    @SerializedName("link_imagem") val linkImagem: String,
-    val tipo: String
+    val id: Int = 0,
+    val receita: String = "",
+    val ingredientes: String = "",
+    val modo_preparo: String = "",
+    val link_imagem: String = "",
+    val tipo: String = ""
 )
