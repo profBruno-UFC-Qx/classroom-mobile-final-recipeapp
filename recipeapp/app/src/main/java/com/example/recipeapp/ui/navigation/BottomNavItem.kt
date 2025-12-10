0736c2dd47b1c299a2290c05d5e16e83deeff208
kauas3228 <kauas3228@gmail.com>
2025-12-10 20:47:59 -0300

    refactor: alterando o nome da rota de minhas receitas no navItem

--- recipeapp/app/src/main/java/com/example/recipeapp/ui/navigation/BottomNavItem.kt
+++ recipeapp/app/src/main/java/com/example/recipeapp/ui/navigation/BottomNavItem.kt
@@ -10,7 +10,7 @@ sealed class BottomNavItem(
     @DrawableRes val iconSelected: Int
 ) {
     object Home : BottomNavItem("home", "Página Inicial", R.drawable.ic_house, R.drawable.ic_house_fill)
-    object MyRecipes : BottomNavItem("my_recipes", "Minhas Receitas", R.drawable.ic_clipboard, R.drawable.ic_clipboard_fill)
+    object MyRecipes : BottomNavItem("my_recipes", "Minhas", R.drawable.ic_clipboard, R.drawable.ic_clipboard_fill)
     object Favorites : BottomNavItem("favorites", "Favoritos", R.drawable.ic_heart, R.drawable.ic_heart_fill)
     object Perfil : BottomNavItem("perfil", "Perfil", R.drawable.ic_person, R.drawable.ic_person)
 }
