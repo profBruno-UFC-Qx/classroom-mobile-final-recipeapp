f462c9d9c61fa650cb7358d2178a108134296553
kauas3228 <kauas3228@gmail.com>
2025-12-16 11:32:43 -0300

    style: atualizando FAB

--- recipeapp/app/src/main/java/com/example/recipeapp/ui/screens/MyRecipesScreen/MyRecipesScreen.kt
+++ recipeapp/app/src/main/java/com/example/recipeapp/ui/screens/MyRecipesScreen/MyRecipesScreen.kt
@@ -1,9 +1,11 @@
 package com.example.recipeapp.ui.screens.MyRecipesScreen
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -22,6 +24,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -59,7 +63,19 @@ fun MyRecipesScreen(
                 containerColor = MaterialTheme.colorScheme.onBackground,
                 contentColor = MaterialTheme.colorScheme.onPrimary
             ){
-                Icon(Icons.Filled.Add, "Adicionar Receita")
+                Row(
+                    modifier = Modifier.padding(12.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ){
+                    Icon(
+                        Icons.Filled.Add, "Adicionar Receita",
+                    )
+                    Text(
+                        text = "Adicionar Receita",
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                }
             }
         }
     ) { padding ->
