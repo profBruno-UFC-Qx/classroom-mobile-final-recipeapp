5ae9e1b8ef1dea41cbb2c5fc7e4d3aa7461f6e80
kauas3228 <kauas3228@gmail.com>
2025-12-16 11:11:22 -0300

    refactor: criando único navHost

--- recipeapp/app/src/main/java/com/example/recipeapp/ui/NavHost.kt
+++ recipeapp/app/src/main/java/com/example/recipeapp/ui/NavHost.kt
@@ -1,7 +1,6 @@
-package com.example.recipeapp.ui.navigation
+package com.example.recipeapp.ui
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
@@ -9,32 +8,59 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.recipeapp.data.model.Recipe
 import com.example.recipeapp.ui.auth.AuthViewModel
+import com.example.recipeapp.ui.navigation.BottomBar
+import com.example.recipeapp.ui.navigation.BottomNavItem
+import com.example.recipeapp.ui.screens.AddRecipeScreen.AddRecipeScreen
 import com.example.recipeapp.ui.screens.FavoritesScreen.FavoritesScreen
+import com.example.recipeapp.ui.screens.FavoritesScreen.FavoritesViewModel
 import com.example.recipeapp.ui.screens.HomeScreen.HomeScreen
+import com.example.recipeapp.ui.screens.LoginScreen.LoginScreen
 import com.example.recipeapp.ui.screens.MyRecipesScreen.MyRecipesScreen
 import com.example.recipeapp.ui.screens.PefilScreen.PerfilScreen
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
-import com.example.recipeapp.data.model.Recipe
-import com.example.recipeapp.ui.screens.AddRecipeScreen.AddRecipeScreen
-import com.example.recipeapp.ui.screens.FavoritesScreen.FavoritesViewModel
 import com.example.recipeapp.ui.screens.RecipeDetailScreen.RecipeDetailScreen
+import com.example.recipeapp.ui.screens.RegisterScreen.RegisterScreen
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun TabsNavHost(navController: NavHostController, authViewModel: AuthViewModel) {
-    val favoritesViewModel: FavoritesViewModel = viewModel()
-    // Auth State
-    val authState = authViewModel.state.collectAsState().value
+fun AppRoot(
+    authViewModel: AuthViewModel,
+) {
+    val authState by authViewModel.state.collectAsState()
+    val navController = rememberNavController()
     val user = authState.user
+    val favoritesViewModel: FavoritesViewModel = viewModel()
 
-    NavHost(navController, startDestination = BottomNavItem.Home.route) {
+    NavHost(
+        navController = navController,
+        startDestination = if (authState.user == null) "login" else "home"
+    ) {
+
+        composable("login") {
+            LoginScreen(
+                authViewModel = authViewModel,
+                navigateToRegister = {
+                    navController.navigate("register")
+                }
+            )
+        }
+
+        composable("register") {
+            RegisterScreen(
+                authViewModel = authViewModel,
+                navigateToLogin = {
+                    navController.popBackStack()
+                }
+            )
+        }
+
         composable(BottomNavItem.Home.route) {
             if (user == null){
                 return@composable
@@ -51,6 +77,7 @@ fun TabsNavHost(navController: NavHostController, authViewModel: AuthViewModel) 
                 )
             }
         }
+
         composable(BottomNavItem.MyRecipes.route) {
             if (user == null){
                 return@composable
@@ -79,6 +106,7 @@ fun TabsNavHost(navController: NavHostController, authViewModel: AuthViewModel) 
                 )
             }
         }
+
         composable(BottomNavItem.Favorites.route) {
             if(user == null){
                 return@composable
@@ -95,14 +123,15 @@ fun TabsNavHost(navController: NavHostController, authViewModel: AuthViewModel) 
                     navController = navController
                 )
             }
-            }
+        }
+
         composable(BottomNavItem.Perfil.route) {
             Scaffold(
                 bottomBar = { BottomBar(navController) }
             ) {
                 PerfilScreen(
                     onLeftClick = {
-                    navController.popBackStack()
+                        navController.popBackStack()
                     }
                 )
             }
@@ -132,3 +161,4 @@ fun TabsNavHost(navController: NavHostController, authViewModel: AuthViewModel) 
         }
     }
 }
+
